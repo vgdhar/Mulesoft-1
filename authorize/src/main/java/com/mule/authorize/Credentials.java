906cdc3c5a3721233715b5892454f41a6d7dbebe
sachin.chakraborty <sachin.chakraborty@capgemini.com>
2018-04-10 15:50:35 +0530

    PoC #2 Completed with API Kit Router/Console & Exception Handling

---
+++ authorize/src/main/java/com/mule/authorize/Credentials.java
@@ -0,0 +1,72 @@
+package com.mule.authorize;
+
+import org.mule.api.MuleEventContext;
+import org.mule.api.lifecycle.Callable;
+import org.mule.extension.annotations.Configuration;
+
+@Configuration
+public class Credentials implements Callable{
+private String userId;
+private String userName;
+private String password;
+
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public Credentials(String userId, String userName, String password) {
+	super();
+	this.userId = userId;
+	this.userName = userName;
+	this.password = password;
+}
+
+public Credentials() {
+	super();
+}
+
+public boolean authorized(String jId, String jName, String jPassword, String pId, String pName, String pPassword) {
+	System.out.println("JID :" + jId + "PID: " + pId + "JName: " + jName + "PName: " + pName + "JPsk: " +  jPassword + "PPsk: " + pPassword);
+	if ((jId.equals(pId)) && (jName.equals(pName)) && (jPassword.equals(pPassword))) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+@Override
+public Object onCall(MuleEventContext eventContext) throws Exception {
+	String jsonId = eventContext.getMessage().getInvocationProperty("jUserId");
+	String jsonName = eventContext.getMessage().getInvocationProperty("jUserName");
+	String jsonPass = eventContext.getMessage().getInvocationProperty("jPassword");
+		
+	Credentials credential = new Credentials();
+	
+	credential.userId = eventContext.getMessage().getInvocationProperty("userId");
+	credential.userName = eventContext.getMessage().getInvocationProperty("userName");
+	credential.password = eventContext.getMessage().getInvocationProperty("password");
+	
+	boolean login = credential.authorized(jsonId, jsonName, jsonPass, credential.userId, credential.userName, credential.password);
+	
+	if (login) {
+		return("Entered credentials are correct. Login successful");	
+	} else {
+		return("Entered credentials are incorrect. Login failed.");	
+	}
+}
+
+}
